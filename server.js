1ffd6be86b71564baba9a2a8e14b00a68a369f20
Ahsan Virani <ahsan.virani@gmail.com>
2017-03-24 07:19:16 +0500

    video 10 done

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-import config, {nodeEnv} from './config'
+import config from './config';
+import apiRouter from './api';
 
-console.log(config, nodeEnv);
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res)=>{
+  res.render('index', {
+    content: 'hahahaha'
+  });
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, ()=>{
+  console.info('express listeingn on port ', config.port);
+});
